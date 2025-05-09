a17125f256239266bcb18345573ba3850338b373
p-canniff <philip.canniff@gmail.com>
2025-05-09 12:23:13 -0400

    Updating to 4.1.1 Realm
    Updating to 4.1.1 Realm

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["GeoQueries"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/realm-cocoa.git", .exact(Version(3, 21, 0))),
+        .package(url: "https://github.com/realm/realm-cocoa.git", .exact(Version(4, 1, 1))),
     ],
     targets: [
         .target(
